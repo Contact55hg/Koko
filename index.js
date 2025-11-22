13c932bda908cdec54837fe0d22b0b63dccf5f78
Kickend <kickendsurs@gmail.com>
2025-11-22 12:30:40 -0300

    Added

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const key = "@Bearer/KeyPass*AuthorizationConfidencionale;;;;Protected@KeystoreT
 
 const mongoAnticheat = require("./anticheat.json");
 
+const banTimesDays = [
+    4, 8, 23, 1, 80, 9, 10, 33, 59, 300, 23, 23, 23, 23, 10, 2, 4, 5, 6, 7, 99, 100
+];
+
 app.use(express.json());
 
 let salas = {
@@ -12,8 +16,25 @@ let salas = {
 };
 
 app.listen(process.env.PORT | 3000, () => {
-    console.log("Servidor rodando na porta 8080");
+    console.log("Servidor rodando na porta " + process.env.PORT | 3000);
 });
+
+// ban user
+
+app.get("/Ban/:id", (req, res) => {
+    var Authorization = req.get("Authorization");
+
+    if (Authorization == key){
+       var json = string(req.params.id);
+
+       var format = JSON.parse(json);
+
+       console.log(format.uid);
+    }else {
+        res.send("Acesso negado");
+        console.log(format.uid);
+    }
+})
 
 // Criar sala
 app.get("/world/create/:id", (req, res) => {
